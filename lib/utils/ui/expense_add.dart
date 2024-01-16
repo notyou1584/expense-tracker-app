ede24c0f3bb090434f4535e378e41066f2c25c4e
noopurjethva <21bca025@sanksaredcation.org>
2024-01-16 12:41:51 +0530

    n

--- lib/utils/ui/expense_add.dart
+++ lib/utils/ui/expense_add.dart
@@ -1,69 +1,146 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ExpenseAddScreen(),
+      title: 'Add Expense',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ExpenseScreen(),
     );
   }
 }
 
-class ExpenseAddScreen extends StatelessWidget {
+class ExpenseScreen extends StatefulWidget {
+  const ExpenseScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ExpenseScreenState createState() => _ExpenseScreenState();
+}
+
+class _ExpenseScreenState extends State<ExpenseScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add Expense'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Expense Category',
-                icon: Icon(Icons.category),
-              ),
+        title: const Text('Add Expense'), // Updated title here
+        centerTitle: true, // Center the title
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            // Navigate back or perform any other action
+          },
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.green,
+          tabs: const [
+            Tab(
+              text: 'Personal',
             ),
-            SizedBox(height: 20),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Amount',
-                icon: Icon(Icons.attach_money),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Description',
-                icon: Icon(Icons.description),
-              ),
-              maxLines: 3,
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Currency',
-                icon: Icon(Icons.attach_money),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Implement the logic to save the expense here
-                print('Expense Saved');
-              },
-              child: Text('Save Expense'),
+            Tab(
+              text: 'Group',
             ),
           ],
         ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          ExpenseForm(type: 'Personal'),
+          ExpenseForm(type: 'Group'),
+        ],
+      ),
+    );
+  }
+}
+
+class ExpenseForm extends StatelessWidget {
+  final String type;
+
+  const ExpenseForm({Key? key, required this.type}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Expense Type: $type',
+              style:
+                  const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 16),
+          const TextField(
+            decoration: InputDecoration(
+              labelText: 'Amount',
+            ),
+            keyboardType: TextInputType.number,
+          ),
+          const SizedBox(height: 16),
+          const TextField(
+            decoration: InputDecoration(
+              labelText: 'Description',
+            ),
+          ),
+          const SizedBox(height: 16),
+          TextField(
+            decoration: const InputDecoration(
+              labelText: 'Date',
+            ),
+            onTap: () async {
+              // Handle the picked date
+            },
+          ),
+          const SizedBox(height: 16),
+          const TextField(
+            decoration: InputDecoration(
+              labelText: 'Category',
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.green),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Expanded(
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle form submission
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      Colors.green, // Set the button color to green
+                ),
+                child: const Text(
+                  '+Add Expense',
+                  style: TextStyle(
+                    fontSize: 24, // Set the font size to make it big
+                    color: Colors.black, // Set the font color to black
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
